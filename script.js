8ceafda31d93954836f4671a2befd885fc2f597d
Nathaniel <nathaniel.c.williams@hotmail.com>
2024-12-25 20:13:05 +1100

    Added audio files and functionality for coffee_shop and fireplace buttons

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
-// Defining a sound
+// Store the button as a variable
+let rain_button = document.querySelector('#rain-button');
+let fireplace_button = document.querySelector('#fireplace-button');
+let coffee_shop_button = document.querySelector('#coffee_shop-button');
 
-
-// Playing sound
-let rain_button = document.querySelector('.sound-button');
-
+// Click event listener for rain_button
 rain_button.addEventListener('click', () => {
+    // Create howl
     const sound = new Howl({
         src: ['./audio/rain-audio.wav'],
         loop: true, //Optional (makes sound loop)
         volume: 1 //Optional (sets the volume from 0 to 1)
     });
-    sound.play();
+    sound.play(); // play sound
+});
 
-})
+// Event listener for fireplace_button
+fireplace_button.addEventListener('click', () => {
+    const sound = new Howl({
+        src: ['./audio/fireplace-audio.mp3'],
+        loop: true, 
+        volume: 1 
+    });
+    sound.play(); 
+});
+
+// Event listener for coffe_shop_button
+coffee_shop_button.addEventListener('click', () => {
+    const sound = new Howl({
+        src: ['./audio/coffee_shop-audio.mp3'],
+        loop: true,
+        volume: 1
+    });
+    sound.play();
+});
