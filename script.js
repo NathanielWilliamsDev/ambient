b4b8ec829f09a1d68d24355233410c1a6c1aa536
Nathaniel <nathaniel.c.williams@hotmail.com>
2024-12-25 19:55:29 +1100

    troubleshooting audio not playing when hosted on github

--- script.js
+++ script.js
@@ -3,14 +3,13 @@
 
 // Playing sound
 let rain_button = document.querySelector('.sound-button');
-console.log(rain_button);
 
 rain_button.addEventListener('click', () => {
     const sound = new Howl({
-        src: ['/audio/rain-audio.wav'],
+        src: ['./audio/rain-audio.wav'],
         loop: true, //Optional (makes sound loop)
         volume: 1 //Optional (sets the volume from 0 to 1)
     });
-    
     sound.play();
+
 })
